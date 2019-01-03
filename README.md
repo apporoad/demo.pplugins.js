0827ea7a0e5f6fa5894d88d58cf62e6bb6e4a8db
apporoad <apporoadina@163.com>
2019-01-03 10:56:18 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # demo.pplugins.js
 demo of pplugins.js
+
+## install
+```bash
+
+npm i -g https://github.com/apporoad/pplugins.js.git
+
+// or
+npm i -g pplugins.js
+
+``` 
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log('hello hello good day')
 
 console.log('details @ demos')
+
+console.log(process.argv)
+
+if(process.argv.length<3){
+    console.log('try node index.js demo1')
+}else{
+
+}
